9e5891b93b37b229ca841763c8d2e4e8e62974bf
kikidun <kierandunbar@gmail.com>
2019-03-05 20:02:53 -0500

    Removed all but the project files

---
+++ build/precache-manifest.e246e3b987f65b516fb3723684cec36f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3ad1e21d83cfd6d0e122",
+    "url": "/static/js/main.3ad1e21d.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "3ad1e21d83cfd6d0e122",
+    "url": "/static/css/main.cd4dca91.chunk.css"
+  },
+  {
+    "revision": "14e9b947f72189ba0acb3a7cb79d9346",
+    "url": "/index.html"
+  }
+];
